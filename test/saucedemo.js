abb2e75982d4a9760d7d5d5aa4fefc184ac29819
Rizal Nugroho <rizalnug2@gmail.com>
2025-02-20 10:17:01 +0700

    implementasi test hooks

--- test/saucedemo.js
+++ test/saucedemo.js
@@ -5,30 +5,30 @@ const edge = require("selenium-webdriver/edge");
 const firefox = require("selenium-webdriver/firefox");
 
 async function saucedemoTest() {
-  describe("Saucedemo Login Test", function () {
-    // menambahkan timeout
-    this.timeout(30000);
+  // deklarasi variabel browsers
+  const browsers = [
+    {
+      name: "chrome",
+      Options: new chrome.Options().addArguments("--headless=new"),
+    },
+    {
+      name: "MicrosoftEdge",
+      Options: new edge.Options().addArguments("--headless"),
+    },
+    {
+      name: "firefox",
+      Options: new firefox.Options().addArguments("--headless"),
+    },
+  ];
 
-    // deklarasi variavel browsers
-    const browsers = [
-      {
-        name: "chrome",
-        Options: new chrome.Options().addArguments("--headless=new"),
-      },
-      {
-        name: "MicrosoftEdge",
-        Options: new edge.Options().addArguments("--headless"),
-      },
-      {
-        name: "firefox",
-        Options: new firefox.Options().addArguments("--headless"),
-      },
-    ];
+  browsers.forEach(async (browser) => {
+    describe("Saucedemo Login Test", function () {
+      let driver;
+      // menambahkan timeout
+      this.timeout(30000);
 
-    it("Should login successfully", async function () {
-      // Script login Success
-      for (browser of browsers) {
-        let driver = await new Builder()
+      beforeEach(async function () {
+        driver = await new Builder()
           .forBrowser(browser.name)
           .setChromeOptions(
             browser.name === "chrome" ? browser.Options : undefined
@@ -41,10 +41,13 @@ async function saucedemoTest() {
           )
           .build();
 
-        try {
-          // fetch halaman
-          await driver.get("https://www.saucedemo.com");
+        // fetch halaman
+        await driver.get("https://www.saucedemo.com");
+      });
 
+      it("Should login successfully", async function () {
+        // Script login Success
+        try {
           // Insert username dan password
           await driver
             .findElement(By.xpath("//input[@id='user-name']"))
@@ -66,35 +69,15 @@ async function saucedemoTest() {
             "Title does not include Swag Labs"
           );
 
-          console.log(`✅ Testing Success on browser: ${browser.name}`);
+          console.log(`✅ Login Success on browser: ${browser.name}`);
         } catch (error) {
           console.error(`❌ Error in ${browser.name}:`, error);
-        } finally {
-          await driver.quit();
         }
-      }
-    });
+      });
 
-    it("Should add product to cart", async function () {
-      // Script success add product to cart
-      for (browser of browsers) {
-        let driver = await new Builder()
-          .forBrowser(browser.name)
-          .setChromeOptions(
-            browser.name === "chrome" ? browser.Options : undefined
-          )
-          .setEdgeOptions(
-            browser.name === "MicrosoftEdge" ? browser.Options : undefined
-          )
-          .setFirefoxOptions(
-            browser.name === "firefox" ? browser.Options : undefined
-          )
-          .build();
-
+      it("Should add product to cart", async function () {
+        // Script success add product to cart
         try {
-          // fetch halaman
-          await driver.get("https://www.saucedemo.com");
-
           // Insert username dan password
           await driver
             .findElement(By.xpath("//input[@id='user-name']"))
@@ -125,18 +108,20 @@ async function saucedemoTest() {
 
           // Assertion menambahkan sukses ditambahkan ke cart
           const cart = await driver
-            .findElement(By.xpath("//a[.='1']"))
+            .findElement(By.className("shopping_cart_badge"))
             .getText();
           assert.strictEqual(cart, "1", "Cart count is not 1");
           await driver.sleep(1000);
 
-          console.log(`✅ Testing Success on browser: ${browser.name}`);
+          console.log(`✅ Add to Cart Success on browser: ${browser.name}`);
         } catch (error) {
           console.error(`❌ Error in ${browser.name}:`, error);
-        } finally {
-          await driver.quit();
         }
-      }
+      });
+
+      afterEach(async function () {
+        await driver.quit();
+      });
     });
   });
 }
